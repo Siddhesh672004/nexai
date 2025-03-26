b502c53db86e64be3f927a5cd0401e4be2a24470
Siddhesh Chaudhari <siddheshavic67@gmail.com>
2025-03-26 20:08:40 +0530

    Header Component Completed with Authentication using Clerk

--- app/page.js
+++ app/page.js
@@ -1,5 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-   <div>Welcome to NexAi</div>
+   <div>Welcome to NexAi
+    <br/>
+    <Button>Hello</Button>
+   </div>
   );
 }
